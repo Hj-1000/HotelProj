c2e51b70f53102f3e4f6a6a6e90939115e2a829e
KIMJONGHAK <blah43992@gmail.com>
2025-01-13 11:17:33 +0900

    레이아웃 페이지 만듬

---
+++ src/main/java/com/ntt/ntt/Controller/StartController.java
@@ -0,0 +1,32 @@
+package com.ntt.ntt.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/admin/executive")
+    public String a(){
+        return "admin/executive";
+    }
+
+    @GetMapping("/admin/executiveRegister")
+    public String b(){
+        return "admin/executiveRegister";
+    }
+
+    @GetMapping("/admin/hotelHeadquarters")
+    public String c(){
+        return "admin/hotelHeadquarters";
+    }
+
+    @GetMapping("/admin/hotelHeadquartersRegister")
+    public String d(){
+        return "admin/hotelHeadquartersRegister";
+    }
+}
