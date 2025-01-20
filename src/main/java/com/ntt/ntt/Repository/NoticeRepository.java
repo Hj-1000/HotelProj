e949d9f74bc33ce129582459be039fcb654a9a3f
hiukyu <vblue7@naver.com>
2025-01-20 17:14:34 +0900

    notice 추가

---
+++ src/main/java/com/ntt/ntt/Repository/NoticeRepository.java
@@ -0,0 +1,12 @@
+package com.ntt.ntt.Repository;
+
+
+import com.ntt.ntt.Entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+
+
+}
