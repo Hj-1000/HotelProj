2e5138f12447301932f0324324d6db5402ad70be
bom <00bom00@naver.com>
2025-02-06 16:54:35 +0900

    2025-02-06 호텔 즐겨찾기 기능 - 즐겨찾기에서 삭제 완료 이제 이미 즐겨찾기에 담긴 중복인지 확인하는 기능 넣을것 지사 상세보기에서 해당 지사의 객실목록 출력(이미지 X)

--- src/main/java/com/ntt/ntt/Entity/LikeHotel.java
+++ src/main/java/com/ntt/ntt/Entity/LikeHotel.java
@@ -1,54 +1,51 @@
 package com.ntt.ntt.Entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedBy;
 
-@Entity //jpa에서 관리를 할수 있습니다. 엔티티매니저
-@Table(name = "likes_hotel")   //jpa를 이용할 때 자동으로 데이터베이스 설정과 데이터베이스 내 테이블을 같이 확인하기때문에 에러 나올수 있음
-// 데이터베이스상 어떤 테이블로 생성할 것인 정보를 담기 위한 어노테이션
+@Entity
+@Table(name = "likes_hotel")
 @Getter
 @Setter
-@ToString
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class LikeHotel extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer likeHotelId; //상품코드
+    private Integer likeHotelId; // 호텔 찜 ID
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "likesId")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private Likes likes;              // 장바구니
+    private Likes likes; // 장바구니
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "hotelId")
-    private Hotel hotel;              //아이템
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotelId", nullable = false)
+    private Hotel hotel; // 호텔
 
     @CreatedBy
     @Column(updatable = false)
-    private String createBy;
+    private String createdBy;
 
-
-    // 카트에 담길 아이템을 참조하는 cartItem
-    public static LikeHotel createLikeHotel (Likes likes, Hotel hotel) {
-        LikeHotel LikeHotel = new LikeHotel();
-        LikeHotel.setLikes(likes);
-        LikeHotel.setHotel(hotel);
-        return LikeHotel;
-
-        // CartItem cartItem = CartItem.createCartItem(cart, item, count);
-        //만들어진 객체로 저장을 할껄?
+    // LikeHotel 객체 생성 메서드
+    public static LikeHotel createLikeHotel(Likes likes, Hotel hotel) {
+        return LikeHotel.builder()
+                .likes(likes)
+                .hotel(hotel)
+                .build();
     }
 
-
-
-
-
+    @Override
+    public String toString() {
+        return "LikeHotel{" +
+                "likeHotelId=" + likeHotelId +
+                ", hotelName='" + hotel +
+                ", createdBy='" + createdBy +'\'' +
+                '}';
+    }
 }
