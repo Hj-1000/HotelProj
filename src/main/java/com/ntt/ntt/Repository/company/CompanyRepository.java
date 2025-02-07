db9e5979ff080808bb0f6e8142b44e00e96ecbe0
JeongHo <jung_0923@naver.com>
2025-02-07 17:19:33 +0900

    관리자페이지 사이드메뉴 호텔본사관리 페이지 링크 수정 본사 상세보기 -> 지사 등록 모달창에도 주소검색 추가 현재 로그인한 사용자가 등록한 본사 목록 가져오는 기능 추가

--- src/main/java/com/ntt/ntt/Repository/company/CompanyRepository.java
+++ src/main/java/com/ntt/ntt/Repository/company/CompanyRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
     Page<Company> findByCompanyNameLike(String keyword, Pageable pageable);  // 회사명 LIKE 검색
     Page<Company> findByCompanyManagerLike(String keyword, Pageable pageable);  // 관리자명 LIKE 검색
     Page<Company> findByCompanyNameLikeOrCompanyManagerLike(String keyword, String keyword1, Pageable pageable);  // 회사명 OR 관리자명 LIKE 검색
 
+    List<Company> findByMember_MemberId(Integer memberId);
 }
