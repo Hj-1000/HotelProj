4926d70a79a0bbbb336c735d4e2cdb9ad5e8543b
JeongHo <jung_0923@naver.com>
2025-02-20 12:30:59 +0900

    목요일 오전 작업 - 알림창 닫기버튼 추가 각종 페이지들(내가만든페이지만) 개선 qna, 공지사항 페이지 아이콘 추가 등

--- src/main/java/com/ntt/ntt/Service/EmailService.java
+++ src/main/java/com/ntt/ntt/Service/EmailService.java
@@ -27,7 +27,14 @@ public class EmailService {
             helper.setFrom("jung04251@gmail.com", "NTT호텔"); // 보내는 사람 이름 설정
             helper.setTo(toEmail);
             helper.setSubject("비밀번호 찾기 코드"); // 이메일 제목
-            helper.setText("비밀번호 찾기 코드 : " + resetCode + "\n \n 코드를 정확하게 입력해주세요.", false); // 이메일 내용
+            helper.setText("<html><body>"
+                    + "<div style='width: 100%; max-width: 410px; padding: 20px; border-radius: 10px; border: 1px solid #ddd; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);'>"
+                    + "<h3 style='color: #4CAF50; font-family: Arial, sans-serif;'>비밀번호 찾기 코드</h3>"
+                    + "<p style='font-size: 16px;'>비밀번호 찾기 코드: <strong style='font-size: 18px; color: #FF5722;'>" + resetCode + "</strong></p>"
+                    + "<p style='font-size: 14px; color: #555;'>코드를 정확하게 입력해주세요.</p>"
+                    + "<p style='font-size: 14px; color: #888;'>감사합니다, NTT호텔</p>"
+                    + "</div></div>"
+                    + "</body></html>", true); // 이메일 내용
 
             emailSender.send(message);  // 이메일 전송
 
