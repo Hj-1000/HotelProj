510ca63fa737c16d09921f1a308dcd7518a21cdc
HJ-1000 <cjsguswhd1369@gmail.com>
2025-02-24 17:02:02 +0900

    관리자의 주문 관리 페이지용 레포지토리, 서비스, 컨트롤러 작성 검색 쿼리문 작성

--- src/main/java/com/ntt/ntt/Repository/ServiceOrderRepository.java
+++ src/main/java/com/ntt/ntt/Repository/ServiceOrderRepository.java
@@ -1,16 +1,39 @@
 package com.ntt.ntt.Repository;
 
+import com.ntt.ntt.Entity.Room;
 import com.ntt.ntt.Entity.ServiceOrder;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Integer> {
+    //검색쿼리
+    // 회원 이름으로 검색
+    Page<ServiceOrder> findByMember_MemberNameLike(String keyword, Pageable pageable);
+    // 회원 이메일로 검색
+    Page<ServiceOrder> findByMember_MemberEmailLike(String keyword, Pageable pageable);
+    // 예약 방으로 검색
+    Page<ServiceOrder> findByReservation_Room_RoomNameLike(String keyword, Pageable pageable);
+    // 주문 날짜로 검색
+    Page<ServiceOrder> findByRegDateLike(LocalDateTime regDate, Pageable pageable);
+
+    //특정 예약에 해당하는 주문을 조회(멤버이름)
+    Page<ServiceOrder> findByReservation_ReservationIdAndMember_MemberNameLike(Integer reservationId, String keyword, Pageable pageable);
+    //특정 예약에 해당하는 주문을 조회(멤버이메일)
+    Page<ServiceOrder> findByReservation_ReservationIdAndMember_MemberEmailLike(Integer reservationId, String keyword, Pageable pageable);
+    //특정 예약에 해당하는 주문을 조회(방이름)
+    Page<ServiceOrder> findByReservation_ReservationIdAndReservation_Room_RoomNameLike(Integer reservationId, String keyword, Pageable pageable);
+    //특정 예약에 해당하는 주문을 조회(주문날짜)
+    Page<ServiceOrder> findByReservation_ReservationIdAndRegDateLike(Integer reservationId, LocalDateTime regDate, Pageable pageable);
+    //검색어 없을 때 사용할 조회
+    Page<ServiceOrder> findByReservation_ReservationId(Integer reservationId, Pageable pageable);
 
     //특정 회원의 구매 이력으로 모두 불러오기
     @Query("select so from ServiceOrder so where so.member.memberEmail = :memberEmail order by so.regDate asc")
@@ -19,8 +42,6 @@ public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Inte
     //특정 회원의 주문갯수 검색 메서드
     @Query("select count(so) from ServiceOrder so where so.member.memberEmail = :memberEmail")
     public Integer totalCount(@Param("memberEmail") String memberEmail);
-
-    public List<ServiceOrder> findByMember_MemberIdOrderByRegDateDesc(String memberEmail, Pageable pageable);
 
     //예약페이지에 룸서비스 주문금액을 추가하기 위해 쿼리문 추가 2025-02-21
     //이 메서드는 reservationId 기준으로 모든 주문 항목의 총 금액을 가져옴.
